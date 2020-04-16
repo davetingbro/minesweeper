1952ceee8dfa56e79a440ebf1751f70130f451ac
davetingbro <davidting3250@gmail.com>
2020-04-16 21:24:31 +1200

    unit test - Initialize method can plant the right number of mines

--- Minesweeper.UnitTests/GameEngineTests.cs
+++ Minesweeper.UnitTests/GameEngineTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Xunit;
 
 namespace Minesweeper.UnitTests
@@ -20,7 +21,15 @@ namespace Minesweeper.UnitTests
         [Fact]
         public void Initialize_WhenCalled_CellArrayContainsCorrectNumberOfMines()
         {
+            var fakeGameBoard = new GameBoard(5, 5, 5);
+            var gameEngine = new GameEngine(fakeGameBoard);
+            gameEngine.Initialize();
 
+            var gameBoard = gameEngine.GameBoard;
+            var result = gameBoard.Cells.Count(c => c.IsMine);
+            var expected = gameBoard.NumOfMines;
+            
+            Assert.Equal(expected, result);
         }
     }
 }
