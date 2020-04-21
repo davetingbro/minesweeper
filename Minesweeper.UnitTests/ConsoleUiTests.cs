b3861fafca39e6b36e22e2404c3f657d77f0893d
davetingbro <davidting3250@gmail.com>
2020-04-21 13:27:14 +1200

    Remove ConsoleUi PrintBoard unit test

--- Minesweeper.UnitTests/ConsoleUiTests.cs
+++ Minesweeper.UnitTests/ConsoleUiTests.cs
@@ -84,33 +84,5 @@ namespace Minesweeper.UnitTests
             Console.Clear();
             Console.SetIn(sr);
         }
-
-        [Fact]
-        public void GameBoardPrintedShouldMatchExpected_WhenPrintBoard()
-        {
-            var gameBoard = new GameBoard(5, 5);
-            var consoleUi = new ConsoleUi();
-            var writer = MockConsoleWriter();
-            
-            consoleUi.PrintBoard(gameBoard);
-            
-            var result = writer.GetStringBuilder().ToString();
-            const string expected = " - - - - -\n" +
-                                    " - - - - -\n" +
-                                    " - - - - -\n" +
-                                    " - - - - -\n" +
-                                    " - - - - -\n";
-            
-            Assert.Equal(expected, result);
-            writer.Close();
-            Console.Clear();
-        }
-
-        private static StringWriter MockConsoleWriter()
-        {
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            return writer;
-        }
     }
 }
