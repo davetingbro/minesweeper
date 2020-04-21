a9d51462dcf2fde8b90085001d74e34c68e0dd3a
davetingbro <davidting3250@gmail.com>
2020-04-22 10:31:05 +1200

    unit test - should call GetDimension once

---
+++ Minesweeper.UnitTests/GameTests.cs
@@ -0,0 +1,27 @@
+using Minesweeper.Interfaces;
+using Moq;
+using Xunit;
+
+namespace Minesweeper.UnitTests
+{
+    public class GameTests
+    {
+        private readonly Game _game;
+        private Mock<IGameEngine> _mockGameEngine;
+        private Mock<IGameUi> _mockGameUi;
+        
+        public GameTests()
+        {
+            _mockGameEngine = new Mock<IGameEngine>();
+            _mockGameUi = new Mock<IGameUi>();
+            _game = new Game(_mockGameEngine.Object, _mockGameUi.Object);
+        }
+
+        [Fact]
+        public void ShouldCallGetDimensionOnce()
+        {
+            _game.Play();
+            _mockGameUi.Verify(ui => ui.GetDimension(), Times.Once);
+        }
+    }
+}
